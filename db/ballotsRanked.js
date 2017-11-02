1502ab43a65aacd7e628a6c13253e290f268f1a2
Adam Lance <adamclance15@gmail.com>
2017-11-02 17:41:37 -0400

    Adds account info and submitted ballot data to account page

--- db/ballotsRanked.js
+++ db/ballotsRanked.js
@@ -1,15 +1,16 @@
 const _ = require('underscore');
 
 let ballotsRanked = [
-    { 
-        id: 1, 
-        name: 'FOR COMMANDER IN CREAM AND VICE ICE', 
-        description: 'Rank candidates in order of choice. Mark your favorite candidate as first choice and then indicate your second and additional back-up choices in order of choice. You may rank as many candidates as you want.', 
+    {
+        id: 1,
+        name: 'FOR COMMANDER IN CREAM AND VICE ICE',
+        description: 'Rank candidates in order of choice. Mark your favorite candidate as first choice and then indicate your second and additional back-up choices in order of choice. You may rank as many candidates as you want.',
         options: [
-            {id: 1, name: 'Reese WithoutASpoon - Democrat for C.I.C Cherry Garcia - Democrat for Vice Ice'}, 
-            {id: 2, name: 'Choco "Chip" Dough - Republican for Vice Ice'}, 
-            {id: 3, name: 'Magic Browny - Independent for C.I.C Phish Food - Independent for Vice Ice'}
-        ]  
+            { id: 1, name: 'Reese WithoutASpoon - Democrat for C.I.C Cherry Garcia - Democrat for Vice Ice' },
+            { id: 2, name: 'Choco "Chip" Dough - Republican for Vice Ice' },
+            { id: 3, name: 'Magic Browny - Independent for C.I.C Phish Food - Independent for Vice Ice' }
+        ],
+        type: 'ranked'
     }
 ];
 
@@ -17,38 +18,46 @@ let ballotsRankedSubmitted = [
     {
         userId: '500ed008-cb0d-4a70-9950-c9b2cd2ba17c',
         ballotId: 1,
+        ballotName: 'FOR COMMANDER IN CREAM AND VICE ICE',
         choices: [
             'Reese WithoutASpoon - Democrat for C.I.C Cherry Garcia - Democrat for Vice Ice',
             'Magic Browny - Independent for C.I.C Phish Food - Independent for Vice Ice',
             'Choco "Chip" Dough - Republican for Vice Ice'
-        ]
+        ],
+        timeStamp: '11/2/2017, 9:02:37 PM'
     },
     {
         userId: 'd2db10db-f24c-428c-b982-a38c2331ba3b',
         ballotId: 1,
+        ballotName: 'FOR COMMANDER IN CREAM AND VICE ICE',
         choices: [
             'Reese WithoutASpoon - Democrat for C.I.C Cherry Garcia - Democrat for Vice Ice',
             'Magic Browny - Independent for C.I.C Phish Food - Independent for Vice Ice',
             'Choco "Chip" Dough - Republican for Vice Ice'
-        ]
+        ],
+        timeStamp: '11/2/2017, 9:02:37 PM'
     },
     {
         userId: '2d5dc4bd-2f0d-47e8-9aa5-97a2111a6b80',
         ballotId: 1,
+        ballotName: 'FOR COMMANDER IN CREAM AND VICE ICE',
         choices: [
-            'Choco "Chip" Dough - Republican for Vice Ice',            
+            'Choco "Chip" Dough - Republican for Vice Ice',
             'Reese WithoutASpoon - Democrat for C.I.C Cherry Garcia - Democrat for Vice Ice',
             'Magic Browny - Independent for C.I.C Phish Food - Independent for Vice Ice',
-        ]
+        ],
+        timeStamp: '11/2/2017, 9:02:37 PM'
     },
     {
         userId: '9bed7059-c3d5-41ae-8c0c-e9a16e0b6f2d',
         ballotId: 1,
+        ballotName: 'FOR COMMANDER IN CREAM AND VICE ICE',
         choices: [
             'Reese WithoutASpoon - Democrat for C.I.C Cherry Garcia - Democrat for Vice Ice',
-            'Choco "Chip" Dough - Republican for Vice Ice',            
+            'Choco "Chip" Dough - Republican for Vice Ice',
             'Magic Browny - Independent for C.I.C Phish Food - Independent for Vice Ice',
-        ]
+        ],
+        timeStamp: '11/2/2017, 9:02:37 PM'
     }
 ];
 
@@ -60,7 +69,7 @@ exports.tallyScores = (ballotsSubmitted) => {
         const candidate = ballot.choices[0];
 
         // If candidate doesn't yet exist push to arr and add 1 vote
-        if (!_.findWhere(candidates, {name: candidate})) {
+        if (!_.findWhere(candidates, { name: candidate })) {
             candidates.push({ name: candidate, votes: 1 });
         } else {
             // Else add 1 vote to existing candidate
@@ -68,7 +77,7 @@ exports.tallyScores = (ballotsSubmitted) => {
             candidates[index].votes++;
         }
     });
-    
+
     // Sort primary candidate array to get first 2 choices
     candidates = _.sortBy(candidates, 'votes').reverse();
 
@@ -87,11 +96,11 @@ exports.tallyScores = (ballotsSubmitted) => {
 
             // Remove eliminated candidate from running
             candidates.pop();
-    
+
             // Distribute eliminated candidates votes
             if (candidate === lastPlace) {
                 for (let i = 1; i < ballot.choices.length; i++) {
-                    if (_.findWhere(candidates, {name: ballot.choices[i].name})) {
+                    if (_.findWhere(candidates, { name: ballot.choices[i].name })) {
                         let index = _.findIndex(candidates, (cand) => cand.name === ballot.choices[i].name);
                         candidates[index].votes++;
 
@@ -114,7 +123,11 @@ exports.tallyScores = (ballotsSubmitted) => {
 };
 
 exports.getBallotById = (id) => {
-    return _.findWhere(ballotsRanked, {id: +id});
+    return _.findWhere(this.ballotsRanked, { id: +id });
+};
+
+exports.getSubmittedByUser = (userId) => {
+    return _.filter(this.ballotsRankedSubmitted, (ballot) => ballot.userId === userId);
 };
 
 exports.getElectionResults = (ballotId) => {
@@ -144,7 +157,20 @@ exports.getAllElectionResults = () => {
 };
 
 exports.checkDuplicates = (userId, ballotId) => {
-    return _.findWhere(this.ballotsRankedSubmitted, {userId, ballotId});
+    return _.findWhere(this.ballotsRankedSubmitted, { userId, ballotId });
+}
+
+exports.submitBallot = (data, res) => {
+    if (this.checkDuplicates(data.userId, +data.ballotId)) {
+        res.send({ msg: 'You may only vote 1 time.' });
+    } else {
+        this.ballotsRankedSubmitted.push({ 
+            userId: data.userId, 
+            ballotId: +data.ballotId, 
+            choices: data.choices,
+            timeStamp: new Date(Date.now()).toLocaleString('en-US', {timeZone: 'UTC'}) 
+        });
+    }
 }
 
 exports.ballotsRanked = ballotsRanked;
--- routes/index.js
+++ routes/index.js
@@ -8,68 +8,63 @@ var DB = require('../db/index');
 
 console.log('users', DB.users.userData);
 
-router.get('/', function(req, res) {
-  res.render('index', { 
-      user: req.user,
-      rankedResults: DB.ballotsRanked.getAllElectionResults() || [],       
-      ballotsRanked: DB.ballotsRanked.ballotsRanked || []
-    });
+router.get('/', function (req, res) {
+	res.render('index', {
+		user: req.user,
+		rankedResults: DB.ballotsRanked.getAllElectionResults() || [],
+		ballotsRanked: DB.ballotsRanked.ballotsRanked || []
+	});
 });
 
-router.get('/login', function(req, res) {
-  if (req.user) {
-    res.redirect('/');
-  } else {
-    res.render('login', { user: req.user });
-  }
+router.get('/login', function (req, res) {
+	if (req.user) {
+		res.redirect('/');
+	} else {
+		res.render('login', { user: req.user });
+	}
 });
 
-router.post('/login', passport.authenticate('local', { failureRedirect: '/' }), function(req, res) {
-  res.redirect('/');
+router.post('/login', passport.authenticate('local', { failureRedirect: '/' }), function (req, res) {
+	res.redirect('/');
 });
 
-router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+router.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
 });
 
-router.post('/register', function(req, res) {
-  DB.users.create(req.body);
+router.post('/register', function (req, res) {
+	DB.users.create(req.body);
 
-  req.login(newUser, function (err) {
-    if ( ! err ){
-        res.redirect('/');
-    } else {
-        //handle error
-    }
-})
+	req.login(newUser, function (err) {
+		if (!err) {
+			res.redirect('/');
+		} else {
+			//handle error
+		}
+	})
 });
 
-router.get('/profile', require('connect-ensure-login').ensureLoggedIn('/?login=true'), function(req, res) {
-  res.render('profile', { user: req.user });
+router.get('/account', require('connect-ensure-login').ensureLoggedIn('/?login=true'), function (req, res) {
+	res.render('account', {
+		user: req.user,
+		ballots: DB.ballotsRanked.getSubmittedByUser(req.user.id)
+	});
 });
 
 
-router.get('/vote/ranked/:id', require('connect-ensure-login').ensureLoggedIn('/?login=true'), function(req, res) {
-  var ballot = DB.ballotsRanked.getBallotById(req.params.id);
+router.get('/vote/ranked/:id', require('connect-ensure-login').ensureLoggedIn('/?login=true'), function (req, res) {
+	var ballot = DB.ballotsRanked.getBallotById(req.params.id);
 
-  res.render('voteRanked', { 
-    user: req.user,
-    ballot: ballot,
-    ballotId: req.params.id
-  });
+	res.render('voteRanked', {
+		user: req.user,
+		ballot: ballot,
+		ballotId: req.params.id
+	});
 });
 
-router.post('/vote/ranked', require('connect-ensure-login').ensureLoggedIn('/?login=true'), function(req, res) {
-  var ballotId = +req.body.id;
-  var choices = req.body.choices;
-  var userId = req.user.id;
-
-  if (DB.ballotsRanked.checkDuplicates(userId, ballotId)) {
-    res.send({msg: 'You may only vote 1 time.'});
-  } else {
-    DB.ballotsRanked.ballotsRankedSubmitted.push({ userId, ballotId, choices });    
-  }
+router.post('/vote/ranked', require('connect-ensure-login').ensureLoggedIn('/?login=true'), function (req, res) {
+	DB.ballotsRanked.submitBallot(req.body, res);
 });
 
 module.exports = router;
